1d1df84854286493146f222b52434bbc76217a38
balasundar-its-bhc <balasundar@bhc.edu.in>
2026-02-09 15:56:13 +0530

    internationalization added

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@/components/ui/button"
+import { useTranslations } from "next-intl";
 
 const HomePage = () => {
+  const t = useTranslations('HomePage');
   return (
     <div className="h-screen flex flex-col gap-4 justify-center items-center">HomePage
-
+      <h1>{t('title')}</h1>
       <Button>This is a text button</Button>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "@/lib/providers/StoreProvider";
+import { NextIntlClientProvider } from "next-intl";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StoreProvider>{children}</StoreProvider>
+        <StoreProvider>
+          <NextIntlClientProvider>
+            {children}
+          </NextIntlClientProvider>
+        </StoreProvider>
       </body>
     </html>
   );
